4e71e8b56960298e73eb025bb5a5522c80f7df5d
MaliciousProgrammer <puttib@yahoo.com>
2020-03-29 13:40:47 +0530

    added ranks

--- js/Player.js
+++ js/Player.js
@@ -3,6 +3,7 @@ class Player {
     this.index = null;
     this.distance = 0;
     this.name = null;
+    this.rank = null;
   }
 
   getCount(){
@@ -22,7 +23,8 @@ class Player {
     var playerIndex = "players/player" + this.index;
     database.ref(playerIndex).set({
       name:this.name,
-      distance:this.distance
+      distance:this.distance,
+      rank:this.rank
     });
   }
 
@@ -31,5 +33,16 @@ class Player {
     playerInfoRef.on("value",(data)=>{
       allPlayers = data.val();
     })
+  }
+  getCarsAtEnd(){
+    var carAtEndRef = database.ref('carsAtEnd');
+    carAtEndRef.on("value", (data)=>{
+      this.rank = data.val();
+    })
+  }
+  static updateCarsAtEnd(rank){
+    database.ref('/').update({
+      carsAtEnd: rank
+    });
   }
 }
